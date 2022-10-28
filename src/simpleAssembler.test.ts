4cb7bda4c648512aa1a1ec80bba31d8588e8d74e
Leo <leo.maedje@gmail.com>
2022-10-28 13:46:30 +0100

    Wrote interpretStep and tests, which pass

--- src/simpleAssembler.test.ts
+++ src/simpleAssembler.test.ts
@@ -1,6 +1,12 @@
-import { simpleAssembler } from "./simpleAssembler";
+import { interpretStep, simpleAssembler } from "./simpleAssembler";
 
-test("simpleAssembler returns correct register", () => {
+test.skip("simpleAssembler returns correct register", () => {
     expect(simpleAssembler(['mov a 5', 'inc a', 'dec a', 'dec a', 'jnz a -1', 'inc a'])).toStrictEqual({'a': 1});
     expect(simpleAssembler(['mov a -10', 'mov b a', 'inc a', 'dec b', 'jnz a -2'])).toStrictEqual({ 'a': 0, 'b': -20 })
+})
+
+test("interpretStep returns the correct object including command, registerKey and value", () => {
+    expect(interpretStep('mov a 5')).toStrictEqual({command: "mov", registerKey: "a", value: "5"});
+    expect(interpretStep('jnz a -1')).toStrictEqual({command: "jnz", registerKey: "a", value: "-1"});
+    expect(interpretStep('inc a')).toStrictEqual({command: "inc", registerKey: "a", value: "1"});
 })
--- src/simpleAssembler.ts
+++ src/simpleAssembler.ts
@@ -9,6 +9,19 @@ value: string,
 }
 
 export function simpleAssembler(program: string[]): Dictionary {
+    const instructions: Instruction[] = program.map(interpretStep);
     const registers: Dictionary = {};
     return registers;
 }
+
+export function interpretStep(step: string): Instruction {
+    const command: string = step.substring(0, 3);
+    const registerKey: string = step[4];
+    let value: string = step.substring(6);
+    if (command === "inc") {
+      value = '1';
+    } else if (command === "dec") {
+      value = '-1';
+    }
+    return { command: command, registerKey: registerKey, value: value };
+  }
