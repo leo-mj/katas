35a177ed094cb542d03c027367a29a370d3c2808
Leo <leo.maedje@gmail.com>
2022-10-28 13:58:43 +0100

    Unskipped simpleAssembler tests, which pass

--- src/simpleAssembler.test.ts
+++ src/simpleAssembler.test.ts
@@ -1,6 +1,6 @@
 import { executeInstructions, interpretStep, simpleAssembler } from "./simpleAssembler";
 
-test.skip("simpleAssembler returns correct register", () => {
+test("simpleAssembler returns correct register", () => {
     expect(simpleAssembler(['mov a 5', 'inc a', 'dec a', 'dec a', 'jnz a -1', 'inc a'])).toStrictEqual({'a': 1});
     expect(simpleAssembler(['mov a -10', 'mov b a', 'inc a', 'dec b', 'jnz a -2'])).toStrictEqual({ 'a': 0, 'b': -20 })
 })
