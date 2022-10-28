75cb6156a692a55ef823693f46fca1669a124c9e
Leo <leo.maedje@gmail.com>
2022-10-28 17:54:43 +0100

    Wrote recursive solution to nextBigger with one large and two small helper functions, all tests pass

--- src/nextBiggerNum.test.ts
+++ src/nextBiggerNum.test.ts
@@ -1,6 +1,7 @@
-import { nextBigger } from "./nextBiggerNum"
+import { nextBigger } from "./nextBiggerNum";
 
-test.skip("nextBigger returns the next largest number consisting of the digits of the input", () => {
-    expect(nextBigger(12)).toBe(21);
-    expect(nextBigger(10)).toBe(-1);
-})
+test("nextBigger returns the next largest number consisting of the digits of the input", () => {
+  expect(nextBigger(12)).toBe(21);
+  expect(nextBigger(10)).toBe(-1);
+  expect(nextBigger(2017)).toBe(2071);
+});
--- src/nextBiggerNum.ts
+++ src/nextBiggerNum.ts
@@ -9,5 +9,53 @@ If the digits can't be rearranged to form a bigger number, return -1
  */
 
 export function nextBigger(n: number): number {
+  const nextBiggerNumberOrN: number = nextBiggerOrN(n);
+
+  if (nextBiggerNumberOrN === n || nextBiggerNumberOrN === Infinity) {
     return -1;
+  }
+
+  return nextBiggerNumberOrN;
+}
+
+export function nextBiggerOrN(n: number): number {
+  let nextBigger = Infinity;
+  const nDigits: string[] = toStringArr(n);
+
+  if (nDigits.length === 1) {
+    // base condition
+    return n;
+  }
+
+  for (let i = 0; i < nDigits.length; i++) {
+    const startingDigit = nDigits[i];
+    let nextBiggerGivenStarting: number = n;
+    const remainingDigits = nDigits.slice(0, i).concat(nDigits.slice(i + 1));
+
+    if (parseInt(startingDigit) > parseInt(nDigits[0])) {
+      const smallestRemainingArrangement = remainingDigits.sort();
+      nextBiggerGivenStarting = toNum(
+        [startingDigit].concat(smallestRemainingArrangement),
+      );
+    } else if (startingDigit === nDigits[0]) {
+      const remainingDigitsOfNAsNum = toNum(nDigits.slice(1));
+      const nextBiggerRemaining: number = nextBiggerOrN(
+        remainingDigitsOfNAsNum,
+      );
+      nextBiggerGivenStarting =
+        n - remainingDigitsOfNAsNum + nextBiggerRemaining;
+    }
+    if (nextBiggerGivenStarting > n && nextBiggerGivenStarting < nextBigger) {
+      nextBigger = nextBiggerGivenStarting;
+    }
+  }
+  return nextBigger;
+}
+
+function toStringArr(n: number): string[] {
+  return n.toString().split("");
+}
+
+function toNum(digits: string[]): number {
+  return parseInt(digits.join(""));
 }
