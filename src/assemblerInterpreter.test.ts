c9c3e62425da917260193bd7b1d89ddf4dbedbd6
Leo <leo.maedje@gmail.com>
2022-10-31 10:54:35 +0000

    Set up assemblerInterpreter framework and test which is skipped

---
+++ src/assemblerInterpreter.test.ts
@@ -0,0 +1,31 @@
+import { assemblerInterpreter } from "./assemblerInterpreter";
+
+test.skip("assemblerInterpreter returns the correct registers", () => {
+    const testInput1:string = `; My first program
+mov  a, 5
+inc  a
+call function
+msg  '(5+1)/2 = ', a    ; output message
+end
+
+function:
+    div  a, 2
+    ret`
+expect(assemblerInterpreter(testInput1)).toBe('(5+1)/2 = 3');
+
+const program_fail = `call  func1
+call  print
+end
+
+func1:
+    call  func2
+    ret
+
+func2:
+    ret
+
+print:
+    msg 'This program should return -1'`
+
+expect(assemblerInterpreter(program_fail)).toBe(-1);
+})
---
+++ src/assemblerInterpreter.ts
@@ -0,0 +1,9 @@
+export function assemblerInterpreter(program: string): string|-1 {
+    const programLines: string[] = program.split("\n");
+    if (!programLines.includes("end")) {
+        return -1;
+    }
+    
+    return -1
+}
+
